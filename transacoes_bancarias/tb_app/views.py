3409756ad4fc609da4affd429774332dc77e71a9
guicosta01 <guiferreiracosta@hotmail.com>
2023-11-10 19:04:03 -0300

    tests

--- transacoes_bancarias/tb_app/views.py
+++ transacoes_bancarias/tb_app/views.py
@@ -76,8 +76,44 @@ def remover_transacoes(request):
                     )
                     estorno.save()
 
-            transacoes.delete()
+            #transacoes.delete()
 
             return Response({'message': 'Transações removidas e registros de estorno criados com sucesso.'}, status=200)
         else:
             return Response({'message': 'Nenhuma transação encontrada para remover.'}, status=404)
+
+
+# Sua API deverá ter um endpoint onde o usuário poderá fornecer um conjunto de registros contendo seu identificador junto com valores a serem editados:
+# A edição de uma transação acontece primeiro fazendo a remoção do mesmo (pela operação de estorno) e então a criação do novo registro (lançamento retificado);
+# Deve ser possível identificar quando um registro foi editado, e o registro mais novo deverá armazenar os identificadores dos registros mais antigos;
+@api_view(['POST'])
+def editarTransacoes(request):
+    if request.method == 'POST':
+        registros_editar = request.data.get('registros_editar', [])
+
+        for registro in registros_editar:
+            identificador = registro.get('identificador')
+            novo_valor = registro.get('novo_valor')
+
+            # Buscar a transação existente pelo identificador
+            transacao_existente = Transacao.objects.get(identificador=identificador)
+
+            # Criar uma transação de estorno para a transação existente
+            estorno = Transacao.objects.create(
+                data_hora_transacao=transacao_existente.data_hora_transacao,
+                modo_transacao=transacao_existente.modo_transacao,
+                categoria=transacao_existente.categoria,
+                nota_observacao=f"Estorno da transação {transacao_existente.identificador}",
+                valor=-transacao_existente.valor,
+                tipo_transacao='estorno'
+            )
+            estorno.save()
+
+            # Atualizar a transação existente com o novo valor e marcá-la como editada
+            transacao_existente.valor = novo_valor
+            transacao_existente.editada = True
+            transacao_existente.save()
+
+        return Response({'message': 'Transações editadas com sucesso.'}, status=200)
+    else:
+        return Response({'message': 'Método não permitido.'}, status=405)
