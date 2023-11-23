f38758d74b8465406c0561dfb3eeacb1812a92e5
guicosta01 <guiferreiracosta@hotmail.com>
2023-11-22 23:04:36 -0300

    filter day

--- transacoes_bancarias/tb_app/filters.py
+++ transacoes_bancarias/tb_app/filters.py
@@ -4,7 +4,9 @@ from .models import Transacao
 
 class TransacaoFilter(django_filters.FilterSet):
     ano = django_filters.NumberFilter(field_name='data_hora_transacao__year')
+    mes = django_filters.NumberFilter(field_name='data_hora_transacao__month')
+    dia = django_filters.NumberFilter(field_name='data_hora_transacao__day')
 
     class Meta:
         model = Transacao
-        fields = ['ano']
+        fields = ['ano', 'mes', 'dia']
